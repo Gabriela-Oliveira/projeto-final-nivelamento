4199986810f5b0a9079530d8d1cb886cac470724
Gabriela Oliveira <eradigitalaquarius@gmail.com>
2020-07-16 10:11:30 -0300

    subindo os primeiros passos da avaliação.

---
+++ projeto-final/javaScript.js
@@ -0,0 +1,152 @@
+/* 6.1 variáveis e constantes, quero exemplos.
+
+        Variáveis -> é o lugar que criamos na memória para armazenar determinadas informações. 
+        Elas podem váriar, ser modificadas.
+
+        ex: var provaFina = 60;
+        
+        Constantes -> São como as variáveis, porém não podem ser alteradas.
+
+        ex: const pularLinha = '<br>';
+
+    
+    6.2 comandos de decisão, quero exemplos.
+
+        É uma decisão, dado uma condição.
+
+        ex:
+        if(idade >= 18){
+            alert('Você é MAIOR de idade!');
+        }else{
+            alert('Você é MENOR de idade!');
+        }
+
+    6.3 loops, quero exemplos.
+
+        É uma instrução para ficar repetindo algum comando, enquanto uma dada condição existir.
+
+        ex:
+        for(i = 0; i < quantidadeNotas; i++){
+                var nota = parseInt(prompt("Digite uma nota: "));
+                notas.push(nota);
+        }
+    6.4 funções, quero exemplos.
+
+        É uma conjunto de de informações a serem executadas (comandos). Ela é muito utilizada para que possamos 
+        chama-la em pontos diferentes do cód.
+
+        ex:
+        function retornarSeParOuImpar(numero){
+        return (numero % 2 != 0) ? 'Impar' : 'Par';
+
+    6.5 escopos, quero exemplos.
+
+        é onde colocamos limites em cód., variáveis, etc. (escopo global - que é acessível de qualquer lugar)
+        (escopo local - que só é acessível dentro daquele escopo)
+
+        ex:
+        local: 
+        function nome() {
+        var nome1 = "Gabriela";
+        }
+
+        global:
+        var nome1 = "Gabriela";
+
+        function nome() {
+        }
+
+    6.6 inputs e outputs, quero exemplos.
+
+        São comando que utilizamos para aprezentar algo para o usuário e também para pegar algo do usuário.
+
+        outputs (saidas)
+        alert()
+        console.log()
+        documento.write()
+
+        inputs (ENTRADAS)
+        prompt()
+
+    6.7 array, quero exemplos.
+
+        É tipo uma coleção de dados.  utilizamos o [], para acrescentar valores.
+
+        ex: var notas = [5,6,7]; 
+
+    6.8 objetos, quero exemplos
+    
+        É parecito com um array, porem listamos coisas que tenham especificações.
+
+        ex: 
+        var pessoa = {[
+                    nome: "Fulano",
+                    idade: 25,
+                    andar: andar,
+                    falar: falar,
+                    correr: correr
+                },
+                {
+                    nome: "Ciclano",
+                    idade: 25,
+                    andar: andar,
+                    falar: falar,
+                    correr: correr
+                }]
+
+    6.9 (camelCase, PascalCase, kibab-case, snake_case).
+
+        ex:
+        camelCase:  a primeira letra da segunda palavra em maiusculo
+        PascalCase: a primeira letra de cada palavra em maiusculo
+        snake_case: tudo minusculo, separando as palavras por _
+        kibab-case: tudo minusculo, separando por - 
+
+    6.10 Diferença entre caminho absoluto e caminho relativo.
+
+        Caminho absoluto -> é o caminho que eu passo fixo c:user/....- de uma pasta na qual tem uma imagem que eu vou usar por exemplo
+        Caminho relativo -> é o caminho que eu estou executando - diretório atual
+        
+    6.11 Diferença entre os operadores &&, ||, == e !=
+
+        && (e) -> ambas as condições tem que ser verdade
+        == (igual) -> ambas tem que ser iguais
+        || (ou) -> uma das opções tem que ser verdade
+        != -> diferente
+
+    6.12 O que é EPER, e para que serve.
+
+        EPER (entender, planejar(fase mais importante), executar e revisar(fase de teste da aplicação))
+        *** serve para organizar melhor as idéias, conseguir entender e executar, facilitar, ter menos erros ***   
+        Começamos dividindo um problemão em vários probleminha -> ai fica bem mais fácil! 
+
+    6.13 Explique o que é Pilha e Fila, quero exemplos.
+
+        pilha -> o primeiro que entra é o ultimo que sai (ex: pilha de livro, o 1º livro a ser posto na pilha será o ultimo a ser retirado )
+        array.pop()    -> remove a ultima posição de uma PILHA.
+        fila  -> o primeiro que entra é o primeiro que sai (ex: fila de banco, o 1º que entra no banco será o primeiro a sair)  
+        rray.shift()  -> tira o primeiro da LISTA.
+
+    6.14 Explique com suas palavras qual a diferença entre html, javascript e css.
+
+        html é uma linguagem para fazermos as marcações é a extrutura.
+        javascript é onde programamos, codificamos.
+        css é para dar estilo ao programa, deixar é bonito, apresentavel ao usuário.
+        
+    
+        
+
+
+
+
+
+        
+
+}
+
+
+
+
+
+*/
+
